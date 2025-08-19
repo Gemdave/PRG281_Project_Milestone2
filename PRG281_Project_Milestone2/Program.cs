8bf6d68d0a04fc358cb3bebb29c36ef047104581
Gemdave <Geralde.jnr@gmail.com>
2025-08-19 12:15:28 +0200

    Fixed User Class

--- PRG281_Project_Milestone2/Program.cs
+++ PRG281_Project_Milestone2/Program.cs
@@ -186,17 +186,6 @@ namespace PRG281_Project_Milestone2
             }
         }
     }
-
-    public class User { }
-    public class Book
-    {
-        public string ISBN { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public bool IsAvailable { get; set; } = true;
-        public void CheckOut() { IsAvailable = false; }
-        public void Return() { IsAvailable = true; }
-    }
     public class Transaction { }
 
 }
--- PRG281_Project_Milestone2/Users/User.cs
+++ PRG281_Project_Milestone2/Users/User.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace PRG281_Project_Milestone2
 {
-    public class User
+    public class User { }
+    public class Book
     {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public void CheckOut() { IsAvailable = false; }
+        public void Return() { IsAvailable = true; }
     }
 }
