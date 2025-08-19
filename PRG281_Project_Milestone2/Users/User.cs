634ba95ea655bc80f5721ae0a3338cb0eff442e0
Gemdave <Geralde.jnr@gmail.com>
2025-08-19 14:57:09 +0200

    Got the code working

--- PRG281_Project_Milestone2/Users/User.cs
+++ PRG281_Project_Milestone2/Users/User.cs
@@ -7,13 +7,4 @@ using System.Threading.Tasks;
 namespace PRG281_Project_Milestone2
 {
     public class User { }
-    public class Book
-    {
-        public string ISBN { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public bool IsAvailable { get; set; } = true;
-        public void CheckOut() { IsAvailable = false; }
-        public void Return() { IsAvailable = true; }
-    }
 }
